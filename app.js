24afcd014bddcbde2e1a31bd5e30d0ca7357e720
mtslck <mats.loock@lnu.se>
2020-01-23 16:41:47 +0100

    Updating before spring 2020

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
  * The starting point of the application.
  *
  * @author Mats Loock
- * @version 1.1.0
+ * @version 1.0.0
  */
 
 'use strict'
 
 const { resolve } = require('path')
-const reviewer = require('./lib/reviewer')
+const { getMovieRatingAverage } = require('./lib/reviewer')
 
 ;(async () => {
   try {
-    const imdbPath = resolve('movies.json')
-    const rottenTomatoesPath = resolve('movies.xml')
+    const imdbPath = resolve(__dirname, 'lib', 'movies', 'movies.json')
+    const rottenTomatoesPath = resolve(__dirname, 'lib', 'movies', 'movies.xml')
 
-    const imdbPromise = reviewer.getMovieRatingAverage(imdbPath)
-    const rottenTomatoPromise = reviewer.getMovieRatingAverage(rottenTomatoesPath)
+    const imdbPromise = getMovieRatingAverage(imdbPath)
+    const rottenTomatoPromise = getMovieRatingAverage(rottenTomatoesPath)
 
     const [imdb, rottenTomato] = await Promise.all([imdbPromise, rottenTomatoPromise])
 
@@ -28,12 +28,13 @@ const reviewer = require('./lib/reviewer')
   }
 })()
 
-// const imdbPath = resolve('movies.json')
-// const rottenTomatoesPath = resolve('movies.xml')
-
+// // ALTERNATIVE SOLUTION
+// const imdbPath = path.resolve(__dirname, 'lib', 'movies', 'movies.json')
+// const rottenTomatoesPath = path.resolve(__dirname, 'lib', 'movies', 'movies.xml')
+//
 // const imdbPromise = reviewer.getMovieRatingAverage(imdbPath)
 // const rottenTomatoPromise = reviewer.getMovieRatingAverage(rottenTomatoesPath)
-
+//
 // Promise.all([imdbPromise, rottenTomatoPromise])
 //   .then(values => {
 //     console.log('Average rating')
